82a25a10dc2abc2cf6d12d9c4de67ff1992d93c3
racnicfer <racnicfer@gmail.com>
2016-03-18 14:15:37 -0700

    plugin SUCCESS!!!!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 normalize.css
     https://necolas.github.io/normalize.css/3.0.2/normalize.css
+    
+MyFadeOverImage plugin
+    http://www.kfsoft.info/MyFadeOverImage/demo.php
--- js/main.js
+++ js/main.js
@@ -1,7 +1,71 @@
+/**
+ * @author Paul Chan / KF Software House 
+ * http://www.kfsoft.info
+ *
+ * Version 0.7
+ * Copyright (c) 2010 KF Software House
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+	
+(function($) {
 
+    var _options = new Array();
+	var _itemId = 0;
+
+	jQuery.fn.MyFadeOverImage = function(options) {
+		_options[_options.length] = $.extend({}, $.fn.MyFadeOverImage.defaults, options);
+		
+		var idx = _options.length-1;
+		var opt = _options[idx];
+		
+		$(this).each(function(){
+			$(this).attr("idx", idx);
+			$(this).fadeTo("fast", opt.normalAlpha);
+			$(this).wrap("<DIV class=tongBg></DIV>");
+			
+			$(this).parent().css("background", opt.normalToneColor);
+			$(this).parent().css("width",  opt.imageWidth+"px");
+			$(this).parent().css("height",  opt.imageHeight+"px");
+			
+		}).mouseenter(function(){
+			$(this).stop();
+			$(this).fadeTo(_options[idx].fadeInSpeed, _options[idx].hoverAlpha);			
+		}).mouseout(function(){
+			$(this).stop();
+			$(this).fadeTo(_options[idx].fadeOutSpeed, _options[idx].normalAlpha);
+		});
+	}
+
+	jQuery.fn.MyFadeOverImage.defaults = {
+		normalAlpha:0.8,
+		hoverAlpha: 1,
+		normalToneColor:"#000",
+		fadeInSpeed: "700",
+		fadeOutSpeed: "700"
+	};
+
+})(jQuery);
+
+
+
+
+$(document).ready(function () {
+  $(".thumbs img").MyFadeOverImage({
+  normalAlpha:0.5,
+  hoverAlpha: 1,
+  normalToneColor:"#fff"
+});
+    
+
+}); 
 
 // Store Background Classes
 var background = ['one', 'two'];
+
+
 console.log(background);
 
 // Random Number Generator
@@ -13,7 +77,6 @@ console.log( randNum );
 $('html').addClass( background[randNum] );
 
 
-$('h2.minititle').lettering();
 
 
 
